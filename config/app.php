768fce9b14b124b3b981a8988bf8a90790b07fa5
Seidjy <samuelpadovangit config --global user.name@hotmail.com>
2017-11-22 00:19:08 -0200

    updating deploy

--- config/app.php
+++ config/app.php
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => 'my encryption key',
 
     'cipher' => 'AES-256-CBC',
 
@@ -121,7 +121,7 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'single'),
+    'log' => 'errorlog',
 
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
