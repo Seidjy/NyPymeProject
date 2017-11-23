2eb1e522a90c870633f73f6d25b573b5c00a097c
Seidjy <samuelpadovangit config --global user.name@hotmail.com>
2017-11-23 14:04:46 -0200

    Correction again

--- app/Http/Controllers/GoalsController.php
+++ app/Http/Controllers/GoalsController.php
@@ -60,8 +60,8 @@ class GoalsController extends Controller
                 'cnpj' => $cnpj,
                 'amountRestrict' => 0,
                 'amountStored' => 0,
-                'created_at' => strtotime('01-01-2015'),
-                'updated_at' => strtotime('01-01-2015'),
+                'created_at' => strtotime('2015-01-01'),
+                'updated_at' => strtotime('2015-01-01'),
             ]
             );
         }
