59c0d9cfee7227226da7b8a6a65cd8c913fd1411
Seidjy <samuelpadovangit config --global user.name@hotmail.com>
2018-01-19 01:53:42 -0200

    Auth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Passport\Passport;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+
+use Laravel\Passport\PassportServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
