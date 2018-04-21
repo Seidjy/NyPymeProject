0fa65791b2d8fb323ab7db781a839c84a27b7f32
Seidjy <samuelpadovangit config --global user.name@hotmail.com>
2018-04-21 18:02:33 -0300

    Atualizações para resposta da api

--- app/Http/Controllers/DealsController.php
+++ app/Http/Controllers/DealsController.php
@@ -40,7 +40,7 @@ class DealsController extends Controller
         $counter = 0;
         foreach ($customer as $client ) {
             $response[$counter] =  [
-                "nome" => $client->name,
+                //"nome" => $client->name,
                 "cpf" => $client->cpf,
                 "cnpj" => $client->cnpj,
                 "pontos" => $client->points,
