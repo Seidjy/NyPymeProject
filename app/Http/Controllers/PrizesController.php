f2c1b6bea13756aeeecb2706ebd0245f54667d4b
Seidjy <samuelpadovangit config --global user.name@hotmail.com>
2018-06-10 23:06:26 -0300

    log_goals

--- app/Http/Controllers/PrizesController.php
+++ app/Http/Controllers/PrizesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use App\Prize;
 use App\LogPrize;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
