117c2abb62476d750ab482e8d008f404acc84862
Seidjy <samuelpadovangit config --global user.name@hotmail.com>
2018-06-10 21:02:01 -0300

    log_goals

---
+++ app/LogPrize.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LogLogin extends Model
+{
+    protected $table = 'log_prize';
+    protected $fillable = [
+        'id', 'ip', 'action','novo_nome', 'antigo_nome', 'novo_preco', 'antigo_preco','cnpj','price'
+    ];
+}
