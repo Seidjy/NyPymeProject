505f14c6542cfae1a5d9c9a32cc15b6629aa0f8b
Seidjy <samuelpadovangit config --global user.name@hotmail.com>
2018-06-11 20:53:37 -0300

    log_goals

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -9,6 +9,7 @@
 	    <ul class="list-group margem">
 	  		<li class="list-group-item"><span class="badge client">{{ $cliente->points }}
 	  		</span>{{ $cliente->cpf }}</li>
+	  		<a href="/participant/{{$cliente->id}}/editar">Editar</a>
 		</ul>
 @endforeach
 		<a href="/deal/debit" class="btn btn-primary btn-contact btn-block">Premiar</a>
