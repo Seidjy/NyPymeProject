1beedc5aace97ce39df40d4613275ed329c18dc6
Seidjy <samuelpadovangit config --global user.name@hotmail.com>
2018-06-12 19:51:15 -0300

    Logs data filter

--- app/Http/Controllers/DealsController.php
+++ app/Http/Controllers/DealsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use App\Deal;
 use App\Http\Controllers\CustomerController;
+use App\Customer;
 use Illuminate\Http\Request;
 use DateTime;
 use Illuminate\Support\Facades\Auth;
