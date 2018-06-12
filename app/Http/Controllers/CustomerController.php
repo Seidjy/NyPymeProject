addf6a0782eb630b93ee2c9a78823f3b53c3cef5
Seidjy <samuelpadovangit config --global user.name@hotmail.com>
2018-06-12 00:47:52 -0300

    log_goals

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use App\Customer;
 use App\LogParticipant;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
