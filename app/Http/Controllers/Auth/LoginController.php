4e8a617f1629ede13d8c3e0e12cba81d96662587
Seidjy <samuelpadovangit config --global user.name@hotmail.com>
2018-06-12 19:15:38 -0300

    Logs data filter

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use App\LogLogin;
 use App\User;
+use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller
 {
